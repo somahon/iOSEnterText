d46826c9998bb185179c4eb19d07286257c15a59
Shane O'Mahony <sleepymole@users.noreply.github.com>
2018-10-23 20:17:35 +0100

    add basic labels and text fields and ensure password field is secure entry

--- iOSEnterText/ViewController.swift
+++ iOSEnterText/ViewController.swift
@@ -1,14 +1,20 @@
-//
-//  ViewController.swift
-//  iOSEnterText
-//
-//  Created by Shane O'Mahony on 23/10/2018.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBAction func doLogin(_ sender: Any) {
+        let username = usernameTextField.text ?? "default"
+        let password = passwordTextField.text ?? "password"
+        
+        outputLabel.text = "User \(username) with password \(password)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
